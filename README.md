83d102faf36cd42cdb4bdea4a556b8f1ad2692a0
lucasmellolm <50436012+lucasmellolm@users.noreply.github.com>
2021-04-05 13:18:13 -0300

    testing main

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # autosearchwikipedia
 Procurar sobre assuntos na Wikipédia automaticamente e criar um resumo em txt. / Search for topics on Wikipedia automatically and create a txt summary.
+
+# Requisitos / Requiriments
+Python 3
+Credentials API`s keys... - Algorithimia, IBM Watson/Cloud (Natural Language Understanding)
--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import requests
+import pathlib
+
 
 def search(topic, language):
     if language == 'pt':
         print('Pesquisando...')
     if language == 'en':
         print('Searching...')
-
-
-    text =
+    text = topic
+    if language == 'pt':
+        print('Pesquisa completada!')
+    if language == 'en':
+        print('Searching Completed!')
     return text
 
-def savetext(text):
-    print('Salvando texto completo...')
 
-def summary():
-    print('Resumindo texto automaticamente! ')
+def savetext(topic, text, language):
+    if language == 'pt':
+        print('Salvando Texto Completo...')
+        filetext = open("TextoCompleto.txt", "a+")
+        filetext.write("\n===================================")
+        filetext.write(f"\n-- Assunto: {topic}")
+        filetext.write(f"\n\nTexto: {text}")
+        filetext.write("\n===================================")
+        filetext.close()
+        print('Salvado com sucesso!')
+    if language == 'en':
+        print('Save Complete Text...')
+        filetext = open("TextComplete.txt", "a+")
+        filetext.write("\n===================================")
+        filetext.write(f"\n-- Topic: {topic}")
+        filetext.write(f"\n\nText: {text}")
+        filetext.write("\n===================================")
+        print('Saved Success!')
+        filetext.close()
 
-def savesummary():
+# def summary():
+    #print('Resumindo texto automaticamente! ')
+
+
+# def savesummary():
 
 
 def main():
@@ -25,13 +48,14 @@ def main():
         language = 'pt'
     if language == 'pt':
         topic = input('O que você deseja pesquisar na Wikipedia? ')
-        summarytopic = input('Sobre oq você deseja fazer resumo? Ex: O que é, Quem é, A história de : ')
+        # summarytopic = input('Sobre oq você deseja fazer resumo? Ex: O que é, Quem é, A história de : ')
     if language == 'en':
         topic = input('What do you want to search on Wikipedia? ')
     if language == 'pt' or language == 'en':
         text = search(topic, language)
-        savetext(text)
-        summary()
+        savetext(topic, text, language)
+    else:
+        print('Erro, linguagem inválida! / Error, language invalid! ')
 
 
 main()
